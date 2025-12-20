2ac74eb5f09aee443c8bf2f774874c14ee61ec41
Eklps <2424052915@qq.com>
2025-12-20 20:26:51 +0800

    更新了关注与共同关注功能

--- src/main/java/com/hmdp/service/IFollowService.java
+++ src/main/java/com/hmdp/service/IFollowService.java
@@ -1,6 +1,7 @@
 package com.hmdp.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Follow;
 
 /**
@@ -12,5 +13,11 @@ import com.hmdp.entity.Follow;
  * @since 2021-12-22
  */
 public interface IFollowService extends IService<Follow> {
+    Result followCommons(Long id);
 
+    //关注与取关
+    Result follow(Long followUserId, Boolean isFollow);
+
+    //判断是否关注
+    Result isFollow(Long followUserId);
 }
